1f15f750abdfd09bd16a9d88e929addf6955fb54
Maxim Borovskiy <patc3rr@mail.ru>
2026-02-27 20:27:50 +0300

    Initial commit

---
+++ Quereria/src/main/java/com/project/Quereria/QuereriaApplication.java
@@ -0,0 +1,13 @@
+package com.project.Quereria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuereriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuereriaApplication.class, args);
+	}
+
+}
